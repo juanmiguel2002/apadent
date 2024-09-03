e098e1059917ad42dd51114bbdcb712b6f9dacff
juanmi <juanmi0802@gmail.com>
2024-09-03 09:51:45 +0200

    don't use this

--- resources/views/clinica/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Lista de Clínicas
-        </h2>
-    </x-slot>
-    @livewire('clinica-show-component')
-
-</x-app-layout>
