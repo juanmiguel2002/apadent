d2ea0ae9c5e5b1c109a3c2a8a19cef1d1c2984e2
juanmi <juanmi0802@gmail.com>
2024-10-07 10:01:25 +0200

    Pacienteshow

--- app/Http/Controllers/PacienteShowController.php
+++ app/Http/Controllers/PacienteShowController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Paciente;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 
 class PacienteShowController extends Controller
 {
@@ -14,19 +11,5 @@ class PacienteShowController extends Controller
     {
         $paciente = Paciente::findOrFail($id);
         return view('paciente-show', ['id' => $id, 'paciente' => $paciente]);
-    }
-
-    public function mostrarImagen($filename)
-    {
-        // Verificar que el usuario tiene rol "admin" o "doctor"
-        if (!Auth::user()->hasRole(['doctor'])) {
-            abort(403, 'No tienes permisos para ver esta imagen.');
-        }
-
-        // Recuperar la imagen del almacenamiento privado
-        $file = Storage::disk('clinicas')->get($filename);
-
-        // Devolver la imagen como una respuesta
-        return response($file, 200)->header('Content-Type', Storage::disk('clinicas')->mimeType($filename));
     }
 }
