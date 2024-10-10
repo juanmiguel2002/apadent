954ede89401ed99935a76cb3c3417c29c39821b3
juanmi <juanmi0802@gmail.com>
2024-10-10 12:30:58 +0200

    enviar mensajes paciente etapa

---
+++ app/Mail/NotificacionMensaje.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Paciente;
+use App\Models\PacienteEtapas;
+use App\Models\TratamientoEtapa;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotificacionMensaje extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $paciente, $etapa, $mensaje, $trat;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Paciente $paciente, $etapa, $trat, $mensaje)
+    {
+        $this->paciente = $paciente;
+        $this->etapa = $etapa;
+        $this->mensaje = $mensaje;
+        $this->trat = $trat;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Nuevo Mensaje',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function build()
+    {
+
+        return $this->subject('Nuevo Mensaje')
+            ->view('emails.mensaje');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
