5451387d3df9e14fbdd2e7c14e187b337b953e06
juanmi <juanmi0802@gmail.com>
2024-10-31 11:26:00 +0100

    new variable url

--- app/Http/Controllers/PacienteHistorial.php
+++ app/Http/Controllers/PacienteHistorial.php
@@ -7,15 +7,28 @@ use App\Models\PacienteTrat;
 
 class PacienteHistorial extends Controller
 {
-    public function index($id)
+    public function index($id, $tratId = null)
     {
+        // Obtener el paciente por su ID
         $paciente = Paciente::findOrFail($id);
-        $tratamiento = PacienteTrat::where('paciente_id', $paciente->id)->with('tratamiento')->get();
-        // dd($tratamiento);
+
+        // Si se pasa un tratamiento, buscar el tratamiento específico del paciente
+        $tratamiento = null;
+        if ($tratId) {
+            $tratamiento = PacienteTrat::where('paciente_id', $paciente->id)
+                                    ->where('trat_id', $tratId)
+                                    ->with('tratamiento')
+                                    ->first();
+        }else{
+            // Si no se pasa un tratamiento
+            $tratamiento = PacienteTrat::where('paciente_id', $paciente->id)->with('tratamiento')->get();
+        }
+
+        // Pasar el paciente y el tratamiento a la vista
         return view('pacientes.paciente-historial', [
             'paciente' => $paciente,
-            'tratamiento' => $tratamiento,
-            // 'id' => $id,
+            'tratamiento' => $tratamiento, // Puede ser null si no se pasa un tratId
+            'tratId' => $tratId, // Este se usará en el componente Livewire
         ]);
     }
 
