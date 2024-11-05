c20b45f3d5eef0993a8b13d29c2d89a0662f8f89
juanmi <juanmi0802@gmail.com>
2024-11-05 11:54:32 +0100

    don't used

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,7 +68,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // 'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         'check.role' => \App\Http\Middleware\CheckUserRole::class,
