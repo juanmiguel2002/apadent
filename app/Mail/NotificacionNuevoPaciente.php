984d3f310d45e802ec10f411a0b7810d4b66e8f8
juanmi <juanmi0802@gmail.com>
2025-02-06 12:18:24 +0100

    nuevo paciente email

--- app/Mail/NotificacionNuevoPaciente.php
+++ app/Mail/NotificacionNuevoPaciente.php
@@ -2,56 +2,37 @@
 
 namespace App\Mail;
 
-use App\Models\Paciente;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 
 class NotificacionNuevoPaciente extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $nombre;
+    public $clinica;
     public $paciente;
+    public $perfilPacienteUrl;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct(Paciente $paciente)
+    public function __construct($user, $paciente,$clinica = null, $perfilPacienteUrl = null)
     {
+        $this->nombre = $user->name;
+        $this->clinica = $clinica;
         $this->paciente = $paciente;
+        $this->perfilPacienteUrl = $perfilPacienteUrl;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Notificacion Nuevo Paciente',
-
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
     public function build()
     {
-        return $this->subject('Nuevo Paciente Creado')
-            ->view('emails.nuevoPaciente')
-            ->with([
-                'pacienteName' => $this->paciente->name,
-                'pacienteApellidos' => $this->paciente->apellidos,
-                'pacienteEmail' => $this->paciente->email,
-                'pacienteTelefono' => $this->paciente->telefono,
-                'pacienteFechaNacimiento' => date('d/m/Y', strtotime($this->paciente->fecha_nacimiento)),
-                'fechaRegistro' => now()->format('d/m/Y H:i'),
-                // 'clinica' => Auth::user()->clinicas,
-            ]);
+        return $this->markdown('emails.nuevoPaciente')
+                    ->with([
+                        'nombre' => $this->nombre,
+                        'clinicaName' => $this->clinica->name,
+                        'perfilPacienteUrl' => $this->perfilPacienteUrl,
+                        'paciente' => $this->paciente,
+                        'fechaRegistro' => now()->format('d/m/Y H:i'),
+                    ]);
     }
 
     /**
