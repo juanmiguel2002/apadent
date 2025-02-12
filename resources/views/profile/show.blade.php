fbf75903d1565a217b0522965d4e21db9daae7ec
juanmi <juanmi0802@gmail.com>
2025-02-12 10:36:26 +0100

    new structure

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,10 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
+@extends('layouts.app')
+@section('pageTitle', 'Perfil')
+@section('header')
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Profile') }}
+    </h2>
+@endsection
+@section('content')
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
@@ -45,4 +46,5 @@
 
         </div>
     </div>
-</x-app-layout>
+@endsection
+
