4a41e747ffed97161f7dbe72e89d5e9731412a60
juanmi <juanmi0802@gmail.com>
2025-02-14 10:59:22 +0100

    include facturas

--- app/Livewire/Carpeta/CarpetaShow.php
+++ app/Livewire/Carpeta/CarpetaShow.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Carpeta;
 
 use App\Models\Carpeta;
+use App\Models\Clinica;
+use App\Models\Factura;
 use Livewire\Component;
 
 class CarpetaShow extends Component
@@ -11,12 +13,30 @@ class CarpetaShow extends Component
     public $showModal = false;
     public $isEditing = false;
     public $subcarpetas;
+    public $archivos;
+    public $facturas;
 
-    public function mount($id) {
-        // $this->carpeta_id = $id;k
+    public function mount($id)
+    {
+        // Obtener la carpeta específica
         $this->carpeta = Carpeta::findOrFail($id);
+
+        // Obtener la clínica a la que pertenece la carpeta
+        $clinica = Clinica::whereHas('carpetas', function ($query) use ($id) {
+            $query->where('id', $id);
+        })->firstOrFail();
+
+        // Cargar subcarpetas y archivos de la carpeta actual
         $this->subcarpetas = $this->carpeta->carpetasHija;
+        $this->archivos = $this->carpeta->archivos ?? collect();
+
+        // Obtener facturas dentro de la carpeta "facturas" de la clínica
+        $this->facturas = Factura::where('clinica_id', $clinica->id)
+                                ->where('carpeta_id', $this->carpeta->id)
+                                ->get();
     }
+
+
     public function render()
     {
         return view('livewire.carpeta.carpeta-show');
