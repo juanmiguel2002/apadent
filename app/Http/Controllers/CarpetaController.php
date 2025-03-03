34eee359e12160efad2ee5a7e17de15288e782dd
juanmi <juanmi0802@gmail.com>
2025-03-03 11:10:51 +0100

    controller carpeta migas de pan

--- app/Http/Controllers/CarpetaController.php
+++ app/Http/Controllers/CarpetaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carpeta;
+use App\Models\Clinica;
+use App\Models\Paciente;
 use Illuminate\Http\Request;
 
 class CarpetaController extends Controller
@@ -16,52 +18,13 @@ class CarpetaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show($id)
     {
         $carpeta = Carpeta::findOrFail($id);
 
-        return view('admin.carpeta.show', compact('id', 'carpeta'));
+        return view('admin.carpeta.show', compact( 'carpeta', 'id'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
