5f858f7d5a3f06ac19a10e3f3a91d15bf4f721d0
juanmi <juanmi0802@gmail.com>
2025-03-10 12:19:20 +0100

    corrección de errores

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
 
 window.onload = function(){
     Livewire.on('clinicaSaved', message => {
--- resources/views/livewire/pacientes/index.blade.php
+++ resources/views/livewire/pacientes/index.blade.php
@@ -162,7 +162,7 @@
     {{-- Añadir paciente --}}
     @can('paciente_create')
         @if($showModal)
-            <x-dialog-modal maxWidth="xl" x-data="{ showModal: @entangle('showModal') }">
+            <x-dialog-modal maxWidth="xl">
                 <div class="relative">
                     <x-slot name="title">
                         <div class="flex justify-between items-center">
