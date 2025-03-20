63dbde964d3c6d32cd3bbbd9c5a76e89cf37e4aa
juanmi <juanmi0802@gmail.com>
2025-03-20 11:02:08 +0100

    correccion de error

--- resources/views/emails/nuevoPaciente.blade.php
+++ resources/views/emails/nuevoPaciente.blade.php
@@ -1,20 +1,20 @@
 @component('mail::message')
-    # Hola, {{ $nombre }}
+# Hola, {{ $nombre }}
 
-    Se ha registrado un nuevo paciente con la siguiente información:
+Se ha registrado un nuevo paciente con la siguiente información:
 
-    - **Nombre:** {{ $paciente->name . " ". $paciente->apellidos }}
-    - **Diagnóstico:** {{ $paciente->tratamientos[0]->name }}
-    - **Registrado:** {{ $fechaRegistro }}
-    - **Clínica:** {{ $clinicaName }}
+- **Nombre:** {{ $paciente->name . " ". $paciente->apellidos }}
+- **Diagnóstico:** {{ $paciente->tratamientos[0]->name }}
+- **Registrado:** {{ $fechaRegistro }}
+- **Clínica:** {{ $clinicaName }}
 
-    La clínica {{ $clinicaName }} ha asignado un nuevo paciente al laboratorio.
-    Haz clic en el botón de abajo para ver su perfil.
+La clínica {{ $clinicaName }} ha asignado un nuevo paciente al laboratorio.
+Haz clic en el botón de abajo para ver su perfil.
 
-    @component('mail::button', ['url' => $perfilPacienteUrl])
-        Ver Perfil del Paciente
-    @endcomponent
+@component('mail::button', ['url' => $perfilPacienteUrl])
+    Ver Perfil del Paciente
+@endcomponent
 
-    Gracias,
-    {{ config('app.name') }}
+Gracias,
+{{ config('app.name') }}
 @endcomponent
